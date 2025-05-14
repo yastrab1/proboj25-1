4e6d0099c25923cf3940ba294ff2f1c5ac912451
yastrab1 <lipka.luki@gmail.com>
2025-05-14 10:39:20 +0200

    combo manager with combo matching

--- combo.py
+++ combo.py
@@ -7,13 +7,24 @@ class ComboManager:
     def __init__(self,beatTimes):
         self.keys = []
         self.beatTimes = beatTimes
+        self.combos = {
+            'fernet':lambda:print("FERNET!!")
+        }
 
     def registerEvent(self,keyCode:int,currentTime):
         for beat in self.beatTimes:
             if abs(beat - currentTime) < constants.COMBO_TOLERANCY/2:
                 self.keys.append(keyCode)
+                self.matchCombos()
                 return
         self.breakCombo()
+
+    def matchCombos(self):
+        keyString = "".join(list(map(pygame.key.name, self.keys)))
+        if keyString in self.combos.keys():
+            self.combos[keyString]()
+            self.breakCombo()
+            return
 
     def breakCombo(self):
         self.keys = []
