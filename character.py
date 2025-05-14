98359ed909e41bcca199e117544eaeca668846ad
KZBMM <betterminecraft7@gmail.com>
2025-05-14 15:54:04 +0200

    .

--- character.py
+++ character.py
@@ -5,9 +5,9 @@ class CharacterTextures:
     def __init__(self, default, attack_default = None, combo_fernet = None, shoot = None, machineGun = None,snipe = None,beer=None):
         self.deault = default
         self.attack_default = attack_default
-        self.shoot=shoot,
-        self.machineGun=machineGun,
-        self.snipe=snipe,
+        self.shoot=shoot
+        self.machineGun=machineGun
+        self.snipe=snipe
         self.beer=beer
         
 
